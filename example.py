cc086b86fd1d6ad099c03b42af8b9147161d38e4
kirdeinega <79926695+kirdeinega@users.noreply.github.com>
2021-03-13 13:05:12 +0300

    append 2 args

--- example.py
+++ example.py
@@ -1,10 +1,11 @@
 from graph_trace import trace, render_trace
 
 y = 10
+x = 10
 
 @trace(label="f1")
 def f1(x):
-    return f2(x) + f3(y) + f4(x)
+    return f2(x) + f3(y) + f4(x, y)
 
 @trace(label="f2")
 def f2(x):
@@ -12,11 +13,11 @@ def f2(x):
 
 @trace(label="f3")
 def f3(y):
-    return f4(y) * 2
+    return f4(y, x) * 2
 
 @trace(label="f4")
-def f4(y):
-    return y ** 3
+def f4(y, x):
+    return y ** x
 
 f1(10)
 render_trace()
