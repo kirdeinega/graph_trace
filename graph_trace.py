b84f9a499f3b3683292dde29b85ce7e4044af8b6
kirdeinega <79926695+kirdeinega@users.noreply.github.com>
2021-03-15 20:17:30 +0300

    no comment

--- graph_trace.py
+++ graph_trace.py
@@ -32,15 +32,22 @@ def trace(label: str):
         return wrapped
     return my_decorator
 
+x = []
+
 def render_trace():
     tree = Digraph()
     open_close = [actions[0]]
     for i in range(1, len(actions) - 2):
         if actions[i].status == ">>>":
-            tree.edge(f"label = {open_close[-1].label}, resalt = {open_close[-1].result}", f"label = {actions[i].label}, resalt = {actions[i].result}", f"args = {actions[i].args}")
+            wer = True
+            tree.edge(f"label = {open_close[-1].label}, resalt = {open_close[-1].result}", f"label = {actions[i].label}, resalt = {actions[i].result}", f"args = {actions[i].args}, kwargs = {actions[i].kwargs}")
             #print(f"label = {open_close[-1].label}, resalt = {open_close[-1].result}", actions[i].label, f"args = {actions[i].args}")
             open_close.append(actions[i])
         elif actions[i].status == "<<<":
+            wer = False
             if len(open_close) > 1:
                 del open_close[-1]
-    tree.render('out/graph_trace.gv', view=True)
+        if wer:
+            x.append(copy.deepcopy(tree))
+    for w in range(len(x)):
+        x[w].render(f'out/graph_trace{[w]}.gv', view = True)
