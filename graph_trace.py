89f66c46c31c2da77ae46e628286f2c1588f1acb
kirdeinega <79926695+kirdeinega@users.noreply.github.com>
2021-03-15 13:19:59 +0300

    no comment

--- graph_trace.py
+++ graph_trace.py
@@ -1,7 +1,7 @@
 from graphviz import Digraph
 from dataclasses import dataclass
 from typing import Any, Dict
-
+import copy
 
 actions = []
 @dataclass
@@ -21,10 +21,12 @@ def trace(label: str):
                 args = args,
                 kwargs = kwargs
             )
+            action_properties_2 = copy.copy(action_properties)
             actions.append(action_properties)
             result = func(*args, **kwargs)
             action_properties.result = result
-            actions.append(action_properties)
+            action_properties_2.status = "<<<"
+            actions.append(action_properties_2)
             return result
         return wrapped
     return my_decorator
@@ -33,11 +35,11 @@ def render_trace():
     tree = Digraph()
     open_close = [actions[0]]
     for i in range(1, len(actions) - 2):
-        if actions[i].label not in open_close:
-            tree.edge(f"label = {open_close[-1].label}, resalt = {open_close[-1].result}", actions[i].label, f"args = {actions[i].args}")
-            print(f"label = {open_close[-1].label}, resalt = {open_close[-1].result}", actions[i].label, f"args = {actions[i].args}")
+        if actions[i].status == ">>>":
+            tree.edge(f"label = {open_close[-1].label}, resalt = {open_close[-1].result}", f"label = {actions[i].label}, resalt = {actions[i].result}", f"args = {actions[i].args}")
+            #print(f"label = {open_close[-1].label}, resalt = {open_close[-1].result}", actions[i].label, f"args = {actions[i].args}")
             open_close.append(actions[i])
-        elif actions[i].label in open_close:
+        elif actions[i].status == "<<<":
             if len(open_close) > 1:
                 del open_close[-1]
     tree.render('out/graph_trace.gv', view=True)
